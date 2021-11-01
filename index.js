ad7914c47a43444770b3332c2ef54fd31c2f97a2
astrogexo <32209658+astrogexo@users.noreply.github.com>
2021-10-31 21:47:49 -0400

    Upgrade discord calls to 13.0 API

--- index.js
+++ index.js
@@ -1,84 +1,73 @@
 /** ------------------------------------------------------------------------------------------------
- * 
- *                         _(`-')    (`-').-> _  (`-')<-.(`-')  
- *                        ( (OO ).-> ( OO)_   \-.(OO ) __( OO)  
- *                        \    .'_ (_)--\_)  _.'    \'-'---.\  
- *                         '`'-..__)/    _ / (_...--''| .-. (/  
- *                         |  |  ' |\_..`--. |  |_.' || '-' `.) 
- *                         |  |  / :.-._)   \|  .___.'| /`'.  | 
- *                         |  '-'  /\       /|  |     | '--'  / 
- *                         `------'  `-----' `--'     `------'  
- *  
- *      Program:  DiscordSidebarPriceBot (DSPB)
+ *
+ *                         _(`-')    (`-').-> _  (`-')<-.(`-')
+ *                        ( (OO ).-> ( OO)_   \-.(OO ) __( OO)
+ *                        \    .'_ (_)--\_)  _.'    \'-'---.\
+ *                         '`'-..__)/    _ / (_...--''| .-. (/
+ *                         |  |  ' |\_..`--. |  |_.' || '-' `.)
+ *                         |  |  / :.-._)   \|  .___.'| /`'.  |
+ *                         |  '-'  /\       /|  |     | '--'  /
+ *                         `------'  `-----' `--'     `------'
+ *
+ *      Program:  DiscordSidebarPriceclient (DSPB)
  *       Author:  Piper
  *                  Discord:  cucurbit
  *                   Reddit:  piper_cucu
  *                  Twitter:  @PiperCucu
  *                   GitHub:  pipercucu
  *
- *  Description:  Discord bot for pulling cryptocurrency price data at intervals and displaying it in the users sidebar
- * 
+ *  Description:  Discord client for pulling cryptocurrency price data at intervals and displaying it in the users sidebar
+ *
  *                                ♡ Made with love in Alabama, USA
- * ------------------------------------------------------------------------------------------------- 
+ * -------------------------------------------------------------------------------------------------
  */
 
- 'use strict'
+'use strict'
 
- const Discord = require('discord.js');
- const rp = require('request-promise');
- const Beans = require('./Beans')
- const bot = new Discord.Client();
- 
- let TICKER = process.env.TOKEN_SYMBOL
- let UPDATE_RATE = process.env.UPDATE_RATE;  // Price update interval in milliseconds
- let guildMeCache = [];
- let UPDATE_INTERVAL;
- 
- // Ready up activities
- bot.on('ready', () => {
-         console.log(`Logged in as ${bot.user.tag}!`);
-         bot.user.setActivity(`😀`);
-         bot.guilds.cache.each(guild => guildMeCache.push(guild.me));
- 
-         // Get update interval from args, default to 1 minute if unpopulated
-         if (typeof UPDATE_RATE == 'undefined') {
-             UPDATE_INTERVAL = 10000; // Default price update rate is once every 10 seconds
-         }
-         else {
-             UPDATE_INTERVAL = parseInt(UPDATE_RATE);
-         }
- 
-         getPrice();
-         setInterval(getPrice, UPDATE_INTERVAL);
-     }
- );
- 
- async function getPrice() {   
- 
-     let currPrice = await Beans.getPrice();
-     showPrice(currPrice); // Update discord bot display
- }
- 
- function showPrice(currPrice) {
- 
-     let showPriceType = '$';  
- 
-     guildMeCache.forEach(guildMe => 
-         guildMe.setNickname(`${TICKER} : ${showPriceType}${currPrice}`));
- 
-     console.log(`${TICKER} $${currPrice} `);
- 
- }
- 
- 
- // New server join event that causes the guild cache to refresh
- bot.on('guildCreate', guild => {
-         bot.guilds.cache.each(guild => guildMeCache.push(guild.me));
-         console.log(`New server has added the bot! Name: ${guild.name}`);
-     }
- );
- 
- bot.login(process.env.BOT_KEY);
- 
- 
- 
+const Discord = require('discord.js')
+const rp = require('request-promise')
+const Beans = require('./Beans')
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] })
+
+let TICKER = process.env.TOKEN_SYMBOL
+let UPDATE_RATE = process.env.UPDATE_RATE // Price update interval in milliseconds
+let guildMeCache = []
+let UPDATE_INTERVAL
+
+// Ready up activities
+clients.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+    client.user.setActivity(`😀`)
+    client.guilds.cache.each(guild => guildMeCache.push(guild.me))
+
+    // Get update interval from args, default to 1 minute if unpopulated
+    if (typeof UPDATE_RATE == 'undefined') {
+        UPDATE_INTERVAL = 10000 // Default price update rate is once every 10 seconds
+    } else {
+        UPDATE_INTERVAL = parseInt(UPDATE_RATE)
+    }
+
+    getPrice()
+    setInterval(getPrice, UPDATE_INTERVAL)
+})
+
+async function getPrice() {
+    let currPrice = await Beans.getPrice()
+    showPrice(currPrice) // Update discord client display
+}
+
+function showPrice(currPrice) {
+    let showPriceType = '$'
+
+    guildMeCache.forEach(guildMe => guildMe.setNickname(`${TICKER} : ${showPriceType}${currPrice}`))
+
+    console.log(`${TICKER} $${currPrice} `)
+}
+
+// New server join event that causes the guild cache to refresh
+client.on('guildCreate', guild => {
+    client.guilds.cache.each(guild => guildMeCache.push(guild.me))
+    console.log(`New server has added the client! Name: ${guild.name}`)
+})
+
+client.login(process.env.client_KEY)
