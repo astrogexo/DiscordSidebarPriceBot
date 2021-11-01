dbe406f74e383fcea72944cec7434fb4b513fec6
astrogexo <32209658+astrogexo@users.noreply.github.com>
2021-10-31 23:49:48 -0400

    Fix switch/case

--- Beans.js
+++ Beans.js
@@ -11,18 +11,17 @@ async function getPrice(dex) {
     let MOVR_BEANS_LP = '0x12d4c0301bd491657fcd4d895b51bce36c30589b' // SolarBeam
 
     switch(dex) {
-        case MoonSwap :
+        case 'MoonSwap' :
             MOVR_BEANS_LP = `0x20472b3cca87f1e8aed70e1cf3ac31e97ed13a1e`
             break;
-        case SolarBeam :
-            break;
-        case FreeRiver :
+        case 'FreeRiver' :
             MOVR_BEANS_LP = `0x120dac3c005aaeb5dc83a6a5379dd7d3f13bc4d1`    
             break;    
-        case Sushi :
+        case 'Sushi' :
             MOVR_BEANS_LP = `0x2b5e714d52860500e6502b29b5df5fb41cb51012`    
             break; 
         default :   
+        // DEFAULT: Use the SolarBeam LP defined above
     }
 
     const MovrBeansPair = new ethers.Contract(MOVR_BEANS_LP, PairABI, provider.movr)
